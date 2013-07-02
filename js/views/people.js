255ff7d71f7954763bf724dda3a40d53b47a4235
colinmegill <colinmegill@gmail.com>
2013-07-02 15:23:23 -0700

    update working, cancel button working on personUpdateForm template and view

--- js/views/people.js
+++ js/views/people.js
@@ -5,8 +5,8 @@ Thorax.View.extend({
     "click button[name='deleteButton']": function(event){
       //event.preventDefault();
       //var modelClicked = $(event.target).model();
-      //this.collection.remove(modelClicked);
-    }
+      //this.collection.remove(modelClicked); -- the faster way to do all this is the handleDelete method implemented below
+    } 
   },
   initialize: function(){
     this.personForm = new Application.Views["personForm"]({
--- js/views/personDetailView.js
+++ js/views/personDetailView.js
@@ -1,3 +1,26 @@
 Thorax.View.extend({
-  name: "personDetailView"
+  name: "personDetailView",
+  events: {
+    "submit form.personEditForm": function(e){
+      e.preventDefault();
+      this.serialize(function(attrs){
+        this.model.save(attrs);
+        this.$('.personEditForm').hide();
+      }, {set: false});
+
+    },
+    "invalid": function(errors){
+      console.log('invalid form input' + errors);
+    },
+  },
+  initialize: function(){
+  },
+  editPerson: function(){
+    this.$('.personEditForm').show();
+    this.populate(); // rather than doing populate here, which we are just showing you... normally you would want to do this in a ready event to avoid any flicker
+  },
+  cancelEdit: function(){
+    this.$('.personEditForm').hide();
+    this.populate(); 
+  }
 })
